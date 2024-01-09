5f781cf85a65fe13c4a1975880a95b028c4066c8
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-01-09 15:09:02 +0530

    Add files via upload

---
+++ TicketManagementApp/DAL/Services/TicketRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using TicketManagementApp.DAL.Interfaces;
+
+namespace TicketManagementApp.DAL.Services
+{
+    public class TicketRepository : Interfaces.ITicketRepository
+    {
+        private TicketDbContext _context;
+
+        public TicketRepository(TicketDbContext context)
+        {
+            _context = context;
+        }
+
+        public Model.TicketModel GetById(int id)
+        {
+            return _context.TicketModels.FirstOrDefault(t => t.TicketId == id);
+        }
+
+        public string GetAll()
+        {
+            string qry = "select* from TicketModels";
+            return qry;
+        }
+
+        public string Add()
+        {
+            string qry = "insert into TicketModels(Title, IsResolved, DueDate)" +
+                "values('";
+            return qry;
+        }
+
+        public string Update()
+        {
+            var query = "update TicketModels set Title='";
+            return query;
+        }
+
+        public string Delete()
+        {
+            var query = "delete from TicketModels where TicketId='";
+            return query;
+        }
+    }
+}
---
+++ TicketManagementApp/Model/Ticket.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace TicketManagementApp.Model
+{
+    public class TicketModel
+    {
+        [Key]
+        public int TicketId { get; set; }
+        public string Title { get; set; }
+        public bool IsResolved { get; set; }
+        public DateTime DueDate { get; set; }
+    }
+}
